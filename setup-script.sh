3c01568b362651303d7e71cc3cc72a589dfc3cfa
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2023-11-18 21:27:51 +0100

    improves script

--- setup-script.sh
+++ setup-script.sh
@@ -1,51 +1,80 @@
 #!/bin/bash
+set -euo pipefail  # Enable strict error handling
+
+# Functions
+
+print_section() {
+    echo "########################################## $1 ##############################################"
+}
+
+# Main Script
+
+# Clone the Linux-scripts repository and move its contents to the current directory
 # git clone https://github.com/Defmon3/linux-scripts && mv linux-scripts/* .
-#chmod +x setup-script.sh
+
+# Make the setup-script.sh executable
+# chmod +x setup-script.sh
+
+# Enable command tracing for debugging
 set -x
-set -e
-# Initial upgrade
-echo "############################## Running update and upgrade ##################################"
-sudo apt -q update && sudo apt -q upgrade -y
-echo "########################################## END ##############################################"
+
+# Set DEBIAN_FRONTEND to noninteractive to make apt assume "yes" for prompts
+export DEBIAN_FRONTEND=noninteractive
+
+# Initial update and upgrade
+print_section "Running update and upgrade"
+sudo apt -q update && sudo apt -q upgrade
 echo ""
 
-echo "############################## PIP Install ##################################"
+# PIP Install
+print_section "PIP Install"
 pip install requests
-echo "########################################## END ##############################################"
 echo ""
 
-echo "############################## Setting up directories ##################################"
-# remove directories
-rm -rf ~/Documents
-rm -rf ~/Music
-rm -rf ~/Pictures
-rm -rf ~/Public
-rm -rf ~/Templates
-rm -rf ~/Videos
-mkdir proj
-echo "########################################## END ##############################################"
+# Setting up directories
+print_section "Setting up directories"
+# Remove directories
+dirs_to_remove=("Documents" "Music" "Pictures" "Public" "Templates" "Videos")
+for dir in "${dirs_to_remove[@]}"; do
+    rm -rf ~/"$dir"
+done
+
+# Create a "proj" directory
+mkdir -p "proj"
 echo ""
 
-#install git
-echo "############################## Installing apps ##################################"
-sudo apt install -y terminator -y
-sudo apt install netcat-traditional
-echo "########################################## END ##############################################"
+# Install applications
+print_section "Installing apps"
+packages_to_install=("terminator" "netcat-traditional")
+for package in "${packages_to_install[@]}"; do
+    sudo apt install "$package" -y -q
+done
 echo ""
 
-wordlists.sh
+# Run additional scripts
+./wordlists.sh
+./proton.sh
+./xmind.sh
+./fix_postgres.sh
+./burp_pro.sh
 
-#complex installations
-proton.sh
-xmind.sh
-fix_postgres.sh
+# Change the default shell to zsh
+print_section "Changing default shell to zsh"
+sudo chsh -s "$(which zsh)"
+echo ""
 
-
-
-
-burp_pro.sh
-sudo chsh -s $(which zsh)
+# Perform system cleanup
+print_section "Performing system cleanup"
 sudo apt autoremove -y
-sudo apt install gvm
+echo ""
 
+# Unset DEBIAN_FRONTEND
+unset DEBIAN_FRONTEND
 
+# Install GVM
+print_section "Installing GVM"
+sudo apt install gvm -y
+echo ""
+
+# Script completed
+echo "Script completed successfully."
